1b19159ddbd7d3344e0451866410f397966643d7
Nikhil <devsotnik@gmail.com>
2022-09-04 11:57:55 +0530

    ss

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 app.use(cors())
 
-app.listen(5000,()=>{
+app.listen(process.env.PORT || 5000,()=>{
     console.log('Listening on port 5000')
 })
 
