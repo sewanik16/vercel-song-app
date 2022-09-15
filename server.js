68048bbdf5b70e4c381dc250e2c25bd7e06fc39c
Nikhil <devsotnik@gmail.com>
2022-09-15 13:25:52 +0530

    m

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const SongController = require('./routes/song-route')
 const ArtstController = require('./routes/artist-route')
 
 app.use(express.json({limit: '50mb'}));
-// app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(cors())
 
 app.listen(process.env.PORT || 5000,()=>{
